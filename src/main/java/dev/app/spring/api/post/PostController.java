c3cf1aa0aa4984a761afa9149ebf66cb8bfd90b7
Anish Panthi <anish.panthi@gmail.com>
2023-12-28 19:46:31 -0600

    Refactored security class

--- src/main/java/dev/app/spring/api/post/PostController.java
+++ src/main/java/dev/app/spring/api/post/PostController.java
@@ -5,6 +5,7 @@ import java.util.Collection;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,16 +16,17 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 @Observed(name = "post-controller")
 @ResponseBody
+@RequestMapping("/api/posts")
 public class PostController {
 
   private final PostService postService;
 
-  @GetMapping("/posts")
+  @GetMapping
   Collection<Post> getPosts() {
     return this.postService.getPosts();
   }
 
-  @GetMapping("/posts/{id}")
+  @GetMapping("/{id}")
   Post getPostById(@PathVariable int id) {
     return this.postService.getPostById(id);
   }
--- src/main/java/dev/app/spring/api/user/UserController.java
+++ src/main/java/dev/app/spring/api/user/UserController.java
@@ -4,6 +4,7 @@ import io.micrometer.observation.annotation.Observed;
 import java.util.Collection;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,11 +15,12 @@ import org.springframework.web.bind.annotation.RestController;
 @ResponseBody
 @RequiredArgsConstructor
 @Observed(name = "user-controller")
+@RequestMapping("/api/users")
 public class UserController {
 
   private final UserRepository userRepository;
 
-  @GetMapping("/users")
+  @GetMapping
   Collection<User> getUsers() {
     return this.userRepository.getUsers();
   }
